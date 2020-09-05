e021d27c8e63316a8a3afde2ddb9fa317e6e4c3a
evilpaper <pellburt@gmail.com>
2020-09-05 09:12:14 +0200

    Setup basic structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Clock from "../Clock/Clock.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Channel 1</h1>
+      <Clock></Clock>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Clock() {
+  return <h1>I'm a clock</h1>;
+}
