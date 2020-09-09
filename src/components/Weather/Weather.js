46a8b30da3de011c716cdca8ea4655f840dbcf80
evilpaper <pellburt@gmail.com>
2020-09-09 15:19:44 +0200

    Populate weather widget with data from open weather map

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -2,9 +2,22 @@ import React, { useState, useEffect } from "react";
 import Card from "../Card/Card.js";
 import Heading from "../Heading/Heading.js";
 
-export default function Weather(props) {
-  const [weather, setWeather] = useState("");
+import Cloud from "../../images/cloud.svg";
+import Hail from "../../images/hail.svg";
+import Moon from "../../images/moon.svg";
+import PartlyMoon from "../../images/partlyMoon.svg";
+import PartlySunny from "../../images/partlySunny.svg";
+import Rain from "../../images/rain.svg";
+import Storm from "../../images/storm.svg";
+import Snow from "../../images/snow.svg";
+import Sun from "../../images/sun.svg";
+
+import WeatherIcon from "../WeatherIcon/WeatherIcon.js";
+
+export default function Weather() {
+  const [weatherDescription, setWeatherDescription] = useState("");
   const [temperature, setTemperature] = useState("");
+  const [weatherIcon, setweatherIcon] = useState("");
 
   useEffect(() => {
     fetch(
@@ -12,16 +25,65 @@ export default function Weather(props) {
     )
       .then((response) => response.json())
       .then((data) => {
-        setWeather(data.weather[0].description);
+        console.log(data);
+        setWeatherDescription(data.weather[0].description);
+        setweatherIcon(data.weather[0].icon);
         setTemperature(data.main.temp);
       });
   }, []);
 
+  const icon = () => {
+    switch (weatherIcon) {
+      case "01d":
+        return Sun;
+      case "01n":
+        return Moon;
+      case "02d":
+        return PartlySunny;
+      case "02n":
+        return PartlyMoon;
+      case "03d":
+        return Cloud;
+      case "03n":
+        return Cloud;
+      case "04d":
+        return Cloud;
+      case "04n":
+        return Cloud;
+      case "09d":
+        return Hail;
+      case "09n":
+        return Hail;
+      case "10d":
+        return Rain;
+      case "10n":
+        return Rain;
+      case "11d":
+        return Storm;
+      case "11n":
+        return Storm;
+      case "13d":
+        return Snow;
+      case "13n":
+        return Snow;
+      case "50d":
+        return Cloud;
+      case "50n":
+        return Cloud;
+
+      default:
+        return null;
+    }
+  };
+
   return (
     <Card>
-      <Heading title="Weather" />
-      <p>{weather}</p>
-      <p>Temperature: {temperature}°C</p>
+      <div className="weather-container">
+        <Heading title="Weather" />
+        <WeatherIcon src={icon()} alt="Hail" />
+        <p>{weatherDescription}</p>
+        <p>Temperature: {temperature}°C</p>
+      </div>
     </Card>
   );
 }
---
+++ src/components/WeatherIcon/WeatherIcon.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WeatherIcon({ src, alt }) {
+  return <img src={src} className="icon" alt={alt} />;
+}
