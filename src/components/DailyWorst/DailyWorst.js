24bef314d354eb695be283c61a4ff48ba126f103
evilpaper <pellburt@gmail.com>
2020-09-17 20:50:48 +0200

    Add Styled Components

--- src/components/DailyWorst/DailyWorst.js
+++ src/components/DailyWorst/DailyWorst.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../Card/Card.js";
 import Heading from "../Heading/Heading.js";
 import DailyWorstImage from "../../images/mick-haupt-iXhbgXrUDSU-unsplash.jpg";
+import Button from "../../atomics/button.js";
 
 export default function DailyBest() {
   return (
@@ -12,6 +13,8 @@ export default function DailyBest() {
         src={DailyWorstImage}
         alt="Today's worst"
       />
+      <Button>I'm a Styled Component</Button>
+      <Button primary>I'm a Styled Component</Button>
     </Card>
   );
 }
