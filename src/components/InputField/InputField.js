49fda01971399d5cbcc09987cb49cf4ad2e55385
evilpaper <pellburt@gmail.com>
2020-09-26 12:25:28 +0200

    Update style on input field

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,4 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  vertical-align: middle;
+  padding: 0.4em;
+  border-radius: 0.5em;
+  outline: 0;
+  border: 1px solid hsla(232, 40%, 40%, 0.6);
+  position: absolute;
+  bottom: 0;
+
+  &::placeholder {
+    font-family: "Inter", sans-serif;
+    color: var(--color-placeholder);
+  }
+
+  &:hover {
+    border: 1px solid var(--color-border-hover);
+  }
+
+  &:focus {
+    outline: none;
+    border-color: var(--color-border-hover);
+  }
+`;
 
 export default function InputField({
   handleChange,
@@ -7,7 +34,7 @@ export default function InputField({
   placeholder,
 }) {
   return (
-    <input
+    <StyledInput
       type="text"
       name="name"
       className="Input-field"
@@ -15,6 +42,6 @@ export default function InputField({
       onChange={handleChange}
       onKeyDown={handleSubmit}
       placeholder={placeholder}
-    />
+    ></StyledInput>
   );
 }
