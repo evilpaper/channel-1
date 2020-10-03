c05eb13f3fa1940ea2365e170d9b68347766c0ee
evilpaper <pellburt@gmail.com>
2020-10-03 11:16:55 +0200

    Refactor list to use styled components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 import { XCircle } from "react-feather";
+
+const StyledList = styled.ul`
+  overflow: scroll;
+`;
+
+const StyledListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledListItemDeleteButton = styled.button`
+  float: right;
+  border: none;
+  background-color: transparent;
+  color: var(--color-light-gray);
+`;
+
+const StyledIconContainer = styled.div`
+  width: 1.2em;
+`;
 
 export default function List({ items, handleDelete }) {
   return (
-    <ul className="list">
+    <StyledList className="list">
       {items.map((item, index) => {
         return (
-          <li key={index} className="list-item">
+          <StyledListItem key={index} className="list-item">
             {item}{" "}
-            <button
+            <StyledListItemDeleteButton
               className="list-item-delete-button"
               onClick={() => handleDelete(index)}
             >
-              <XCircle className="list-item-delete-button-icon" />
-            </button>
-          </li>
+              <StyledIconContainer>
+                <XCircle className="list-item-delete-button-icon" />
+              </StyledIconContainer>
+            </StyledListItemDeleteButton>
+          </StyledListItem>
         );
       })}
-    </ul>
+    </StyledList>
   );
 }
